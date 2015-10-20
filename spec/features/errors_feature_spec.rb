8a6c3b7891515b053c547417e17b88b5f9531dd1
Ben <ben.c.hawker@gmail.com>
2015-10-20 10:03:35 +0100

    Added Errors Controller for 404 error handling

---
+++ spec/features/errors_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'errors' do
+
+  context '404' do
+	  scenario 'user navigates to an invalid path' do
+	    visit '/qewrer'
+	    expect(page).to have_content('Houston we have a problem')
+	  end
+	end
+end
