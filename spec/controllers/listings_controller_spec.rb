978a02a9232dd9fdbbec5780a5bbd4aa50eb1afd
Ben <ben.c.hawker@gmail.com>
2015-11-14 21:56:48 +0800

    Listings controller spec index and show

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ListingsController, type: :controller do
 	describe "#show" do
-
 		let(:user) { FactoryGirl.create(:user) }
 	  let!(:category) { FactoryGirl.create(:category) }
 	  let!(:listing) { FactoryGirl.create(:listing, :les_paul, user: user, category: category) }
@@ -18,6 +17,17 @@ RSpec.describe ListingsController, type: :controller do
       expect(assigns(:listing)).to eq(listing)
     end
   end
+
+  describe "#index" do
+	  let!(:user) { FactoryGirl.create(:user) }
+	  let!(:category) { FactoryGirl.create(:category) }
+	  let!(:listing) { FactoryGirl.create(:listing, :les_paul, user: user, category: category) }
+  
+	  it "assigns all articles as @articles" do
+    	get :index
+    	assigns(:listings).should eq([listing])
+	  end
+	end
 end
 
 
