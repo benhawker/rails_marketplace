44e5e50f869e83d7a93e5f9acdd5b490f2eb40a6
Ben <ben.c.hawker@gmail.com>
2015-12-05 12:36:46 +0800

    Adds to the messages controller and index view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,43 @@
 class MessagesController < ApplicationController
+	before_action do
+		@inquiry = Inquiry.find(params[:inquiry_id])
+  end
+
+	def index
+		@messages = @inquiry.messages
+	  if @messages.length > 10
+	  	@over_ten = true
+	  	@messages = @messages[-10..-1]
+	  end
+	  
+	  if params[:m]
+	  	@over_ten = false
+	  	@messages = @inquiry.messages
+	  end
+	 
+	 	if @messages.last
+	  	if @messages.last.user_id != current_user.id
+	  		@messages.last.read = true;
+	  	end
+	 	end
+
+ 		@message = @inquiry.messages.new
+ 	end
+
+	def new
+	 @message = @inquiry.messages.new
+	end
+	
+	def create
+	 @message = @inquiry.messages.new(message_params)
+	 if @message.save
+	  redirect_to inquiry_messages_path(@inquiry)
+	 end
+	end
+	
+	private
+	
+	def message_params
+	  params.require(:message).permit(:body, :user_id)
+	end
 end
