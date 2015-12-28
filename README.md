fc130c72658cbc831b8bca115a8578227db8587d
Ben <ben.c.hawker@gmail.com>
2015-12-28 22:14:48 +0800

    Additions

--- README.md
+++ README.md
@@ -2,15 +2,12 @@
 [![Coverage Status](https://coveralls.io/repos/benhawker/rails_marketplace/badge.svg?branch=master&service=github)](https://coveralls.io/github/benhawker/rails_marketplace?branch=master)
 
 
-#7 - A Rails marketplace for listing musical instruments.
+Rails Project #7 - A Rails marketplace for listing musical instruments.
 
 ```
 Aims:
-- #1 - Learn, learn & learn.
-- Ensure 90%+ test coverage.
+- #1 - Learn, learn & learn some more.
+- Aim for 90%+ test coverage.
 - Use of the Neat & Bourbon frameworks for the first time in a project.
-- Use of the Mailboxer gem with customisation to enable messaging between seller and buyer linked to a listing.
-- Use Service Object's where appropriate.
-- Use mailers/Integrate 3rd party mail tool to send mails to registered users.
 ```
 
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -12,14 +12,22 @@ RSpec.describe Offer, type: :model do
   #Attribute validations
   it { should validate_presence_of(:price).with_message("Your offer must have a price!") }
 
+	let!(:user_one) { FactoryGirl.create(:user, :ben) }
+	let!(:user_two) { FactoryGirl.create(:user, :bob) }
+	let!(:category) {FactoryGirl.create(:category) }
+	let(:listing) { FactoryGirl.create(:listing, category: category, user: user_one) }
+	let(:offer) { FactoryGirl.create(:offer, user: user_two, listing: listing) }
+
+  it "cannot be created without a user, a listing & a price" do
+ 		expect{ Offer.create(price: 4.99, listing: listing)}.not_to change{Offer.count}
+  end
+
+ 	it "will be created with a user, a listing & a price" do
+ 		expect{ Offer.create(price: 4.99, user: user_one, listing: listing)}.to change{Offer.count}
+  end
+
   context 'as a buyer' do
 	  context 'making an offer' do
-	    let!(:user_one) { FactoryGirl.create(:user, :ben) }
-	    let!(:user_two) { FactoryGirl.create(:user, :bob) }
-	    let!(:category) {FactoryGirl.create(:category) }
-	    let(:listing) { FactoryGirl.create(:listing, category: category, user: user_one) }
-	    let(:offer) { FactoryGirl.create(:offer, user: user_two, listing: listing) }
-
 	    it 'is accepted if offer is valid price' do
 	    	offer.price = 4.99
 	    	expect(offer).to be_valid
@@ -31,11 +39,14 @@ RSpec.describe Offer, type: :model do
 	    end
 
 	    it 'user cannot make a new offer if they currently have an existing offer on the listing' do
-
+				# expect{ Offer.build(price: 4.99, user: user_two, listing: listing)}.not_to change{Offer.count}
 	    end
 
 	    it 'raises an error if user has already made 5 offers on a listing' do
-
+	    	4.times do
+	    		FactoryGirl.create(:offer, user: user_two, listing: listing)
+	    	end
+	    	expect{ FactoryGirl.create(:offer, user: user_two, listing: listing) }.
 	    end
 
 	    it 'raises an error if user has more than 15 live offers' do
