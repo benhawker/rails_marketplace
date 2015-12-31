2f641a01a944269fe6d3a7938354f4010104159d
Ben <ben.c.hawker@gmail.com>
2015-12-31 20:08:09 +0800

    Feedback factory

--- spec/factories/feedbacks.rb
+++ spec/factories/feedbacks.rb
@@ -1,6 +1,18 @@
 FactoryGirl.define do
   factory :feedback do
-    
+    association :buyer, factory: :user
+    association :seller, factory: :user
+    association :listing
+    rating { true }
+    comment { Faker::Lorem.sentence }
+  end
+
+  trait :positive do
+  	rating { true }
+  end
+
+  trait :negative do
+  	rating { false }
   end
 
 end
