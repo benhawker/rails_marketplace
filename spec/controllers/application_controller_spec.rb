cc8adce7186b4f71df1d2a5bc7a0ed49ce5575b7
Ben <ben.c.hawker@gmail.com>
2015-12-31 17:00:30 +0800

    Application controller specs

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,19 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
 
-	it "raises RecordNotFound when not found" do
-		# get :show, :id => 999
-  #   assert_raises(ActiveRecord::RecordNotFound) do
-  #     get :show, :id => 1234
-  #   end
-
-    # expect { get :show, id: 999	}.to raise_error(ActiveRecord::RecordNotFound)
-
-    # get :show, :id => 245352
-    # response.response_code.should == 404
-
-    get :show, :id => 245352
-    expect(response).to have_http_status(:not_found)
-  end
+		controller(ApplicationController) do
+    	def index
+      	render :nothing => true
+    	end
+  	end
 
 end
