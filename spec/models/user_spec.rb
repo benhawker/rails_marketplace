10589258fe30e9a4938236bd8d8a469051eb0919
Ben <ben.c.hawker@gmail.com>
2016-01-01 17:41:38 +0800

    Adds to suggested listings spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,40 +7,49 @@ RSpec.describe User, type: :model do
   it { should have_many(:messages) }
   it { should have_many(:watched_listings).through(:watches).dependent(:destroy) }
   it { should have_many(:offers).dependent(:destroy) }
-
   it { should have_many(:given_feedbacks_as_seller) }
   it { should have_many(:given_feedbacks_as_buyer) }
   it { should have_many(:received_feedbacks_as_seller) }
   it { should have_many(:received_feedbacks_as_buyer) }
 
+  let!(:user_one) { FactoryGirl.create(:user) }
+  let!(:category) {FactoryGirl.create(:category) }
+  let!(:listing_one) { FactoryGirl.create(:listing, user: user_one, category: category) }
+  let!(:listing_two) { FactoryGirl.create(:listing, user:  user_one, category: category) }
+  
   context 'watching a listing' do
-    let!(:user_one) { FactoryGirl.create(:user) }
-    let!(:category) {FactoryGirl.create(:category) }
-    let!(:listing_one) { FactoryGirl.create(:listing, user: user_one, category: category) }
-    let!(:listing_two) { FactoryGirl.create(:listing, user:  user_one, category: category) }
-
     it 'counts the correct number of listings user is watching' do
       user_one.watched_listings << listing_one
       user_one.watched_listings << listing_two
       expect(user_one.watched_listings.count).to eq 2
     end
+  end
 
-    context "user role" do
-      it 'user has a role of standard by default' do 
-        expect(user_one.role).to eq ("standard")
-      end
+  context "user role" do
+    it 'user has a role of standard by default' do 
+      expect(user_one.role).to eq ("standard")
+    end
 
-      it 'user is invalid if role is not found within User::ROLES' do
-        user_one.role = "bob"
+    it 'user is invalid if role is not found within User::ROLES' do
+      user_one.role = "bob"
+      expect(user_one).to_not be_valid
+      expect(user_one.errors[:role]).to include "is not included in the list"
+    end
+
+    it 'user is invalid if role is blank' do
+      user_one.role = nil
+      expect(user_one).to_not be_valid
+      expect(user_one.errors[:role]).to include "is not included in the list"
+    end
+  end
+
+  context "with an invalid email address format" do
+    it "should not be valid" do
+      ["ben_ hawker@XYZ.com", "ben++hawker@gmail.com", "ben....hawker@gmail.com"].each do |format|
+        user_one.email = format
         expect(user_one).to_not be_valid
-        expect(user_one.errors[:role]).to include "is not included in the list"
-      end
-
-      it 'user is invalid if role is blank' do
-        user_one.role = nil
-        expect(user_one).to_not be_valid
-        expect(user_one.errors[:role]).to include "is not included in the list"
       end
     end
   end
+
 end
--- spec/services/suggested_listings_spec.rb
+++ spec/services/suggested_listings_spec.rb
@@ -7,4 +7,11 @@ describe SuggestedListings do
     @user = FactorlGirl.create(:user)
   end
 
+  it "suggests active listings of relevance linked to the listing being viewed" do
+    listing = FactoryGirl.create(:listing)
+    alternate_listing = FactoryGirl.create(:listing, user: user, destination: room.category)
+
+    expect(SuggestedListings.suggest_relevant_listings(listing)).to include(alternate_listing)
+  end
+
 end
