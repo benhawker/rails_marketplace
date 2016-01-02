8c85363ccfb5010c354d8f812be9c8cdd2c533c0
Ben <ben.c.hawker@gmail.com>
2016-01-02 12:09:27 +0800

    Adds location model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160101095152) do
+ActiveRecord::Schema.define(version: 20160102040910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,11 @@ ActiveRecord::Schema.define(version: 20160101095152) do
 
   add_index "listings", ["category_id"], name: "index_listings_on_category_id", using: :btree
   add_index "listings", ["user_id"], name: "index_listings_on_user_id", using: :btree
+
+  create_table "locations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text     "body"
