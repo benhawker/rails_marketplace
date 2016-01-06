0b053f3a1b2f14108a59784ac6caad993ceacac8
Ben <ben.c.hawker@gmail.com>
2016-01-06 22:34:20 +0800

    Adds 500 internal server error route

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,5 +3,9 @@ class ErrorsController < ApplicationController
   def not_found
     render :status => 404
   end
-    
+
+  def server_error
+  	render :status => 500
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -36,5 +36,14 @@ Rails.application.routes.draw do
     get "/#{action}", to: "pages##{action}", as: "#{action}_page"
   end
 
+  # Errors
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#error_500'
+
+  unless Rails.application.config.consider_all_requests_local
+    get '*not_found', to: 'errors#not_found'
+  end
+
   # get "*any", via: :all, to: "errors#not_found"
+
 end
