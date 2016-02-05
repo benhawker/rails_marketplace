55bf9d3b33d8263fa0953a24a838e476ea00ae94
Ben <ben.c.hawker@gmail.com>
2016-02-05 11:02:40 +0800

    Fix suggested listings spec

--- spec/services/suggested_listings_spec.rb
+++ spec/services/suggested_listings_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 describe SuggestedListings do
 
-  before do
-    @listing = FactoryGirl.create(:listing)
-    @user = FactorlGirl.create(:user)
-  end
+	let(:category) { FactoryGirl.create(:category) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:listing) { FactoryGirl.create(:listing, user: user, category: category) }
+
+	let(:user) { FactoryGirl.create(:user, :ben) }
+  let(:alternate_listing) { FactoryGirl.create(:listing, user: user, category: category) }
 
   it "suggests active listings of relevance linked to the listing being viewed" do
-    listing = FactoryGirl.create(:listing)
-    alternate_listing = FactoryGirl.create(:listing, user: user, destination: room.category)
-
     expect(SuggestedListings.suggest_relevant_listings(listing)).to include(alternate_listing)
   end
 
